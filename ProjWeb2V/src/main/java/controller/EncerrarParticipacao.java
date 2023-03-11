bc1c3c5e8dde8ceeb74835fe5d2018964c956813
Pablo de Oliveira <pablo.oliveira.xavier@gmail.com>
2023-03-11 14:31:25 -0300

    adicionando css

--- ProjWeb2V/src/main/java/controller/EncerrarParticipacao.java
+++ ProjWeb2V/src/main/java/controller/EncerrarParticipacao.java
@@ -8,7 +8,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Servlet implementation class EncerrarParticipacao
@@ -33,9 +36,12 @@ public class EncerrarParticipacao extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(true);
+
+		Aluno nome = (Aluno) session.getAttribute("user");
 		Banco banco = new Banco();
-		Aluno aluno = new Aluno();
-		ParticipacaoProjeto participacao = new ParticipacaoProjeto();
+		List<ParticipacaoProjeto> lista = banco.getProjetos();
+		
 		
 		
 		
